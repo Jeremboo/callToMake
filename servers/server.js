3095b535eaa72b05452c4168215d11127e77c9c9
Mathis Biabiany <mathis.biabiany@gmail.com>
2016-01-13 14:53:04 +0100

    Tests raspberry #Sound

--- servers/server.js
+++ servers/server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var say = require('say'),
+var say = require('say');
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
