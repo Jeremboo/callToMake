ad475547d52bdfa07a63da85f359b7c677da797a
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-01-14 18:10:18 +0100

    remove useless files

--- servers/components/MyEmitter.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events');
-const util = require('util');
-
-function MyEmitter() {
-  EventEmitter.call(this);
-}
-util.inherits(MyEmitter, EventEmitter);
-
-module.exports = new MyEmitter();
